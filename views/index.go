631d7077a68b8483f59c93be1ef21f5c45d5cc52
Ville Kaikkonen <ville.kaikk@gmail.com>
2023-11-06 11:54:07 +0200

    Add logging to the Index handler

--- views/index.go
+++ views/index.go
@@ -1,13 +1,16 @@
 package views
 
 import (
-	"text/template"
+	"log"
 	"net/http"
+	"text/template"
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
+	log.Println("GET Index")
 	tmpl:= template.Must(template.ParseFiles("templates/index.html"))
 	tmpl.Execute(w, nil)
 }
