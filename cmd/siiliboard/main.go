fa4400152d483fff347c039482ad460345f1472c
Ville Kaikkonen <ville.kaikk@gmail.com>
2023-11-08 11:20:21 +0200

    Setup logging to files

--- cmd/siiliboard/main.go
+++ cmd/siiliboard/main.go
@@ -3,27 +3,39 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	"path/filepath"
 	"time"
 
 	"github.com/joho/godotenv"
 
 	"siiliboard/internal/database"
 	"siiliboard/internal/routes"
+	"siiliboard/internal/utils"
 )
 
-var  default_port string = "8080"
+var default_port string = "8080"
+var DEBUG bool = false
 
 func main() {
 
-	setupLogger()
-	err := godotenv.Load(".env")
+	rootPath, err := filepath.Abs(".")
 	if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+		log.Fatal(err)
+	}
+	logFile := setupLogger(rootPath)
+
+	defer logFile.Close()
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	resolveEnv()
 
 	port, succ := os.LookupEnv("PORT_SERVE")
 
@@ -37,7 +49,7 @@ func main() {
 		log.Fatalf("Unable to resolve database settings - %s\n", err.Error())
 	}
 
-    db, err := database.Connect(s)
+	db, err := database.Connect(s)
 
 	if err != nil {
 		log.Fatalf("Error opening database connection - %v\n", err.Error())
@@ -46,54 +58,84 @@ func main() {
 	defer db.Close()
 	database.CreateTables()
 
-	router := routes.NewRouter()
+	router := routes.NewRouter(rootPath)
 	port = fmt.Sprintf(":%v", port)
 
 	log.Printf("Starting the server at http:127.0.0.1%v\n", port)
 
-    log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
 
-func setupLogger() {
-	
+func setupLogger(rootPath string) *os.File {
+
 	log.SetFlags(log.Lshortfile)
 	datetime := "2006-01-02 15:04:05: "
 	log.SetPrefix(time.Now().Format(datetime))
+	logFileName := time.Now().Format("2006-01-02.log")
+	logDirPath := filepath.Join(rootPath, "log")
+	logFileFullPath := filepath.Join(logDirPath, logFileName)
+
+	_, err := os.Stat(logDirPath)
+	if os.IsNotExist(err) {
+		err := os.Mkdir(logDirPath, os.ModePerm)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	f, err := os.OpenFile(logFileFullPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
+	return f
 }
 
 func resolveDBSettings() (*database.DBSettings, error) {
-	
+
 	pq_user, succ := os.LookupEnv("PQ_USER")
-	if !succ || len(strings.TrimSpace(pq_user)) == 0 {
+	if !succ || utils.IsEmpty(pq_user) {
 		return nil, errors.New("PQ_USER not defined")
 	}
 
 	pq_passwd, succ := os.LookupEnv("PQ_PASSWD")
-	if !succ || len(strings.TrimSpace(pq_passwd)) == 0 {
+	if !succ || utils.IsEmpty(pq_passwd) {
 		return nil, errors.New("PQ_PASSWD not defined")
 	}
 
 	pq_db, succ := os.LookupEnv("PQ_DB")
-	if !succ || len(strings.TrimSpace(pq_db)) == 0 {
+	if !succ || utils.IsEmpty(pq_db) {
 		return nil, errors.New("PQ_DB not defined")
 	}
 
 	pq_addr, succ := os.LookupEnv("PQ_ADDR")
-	if !succ || len(strings.TrimSpace(pq_addr)) == 0 {
+	if !succ || utils.IsEmpty(pq_addr) {
 		return nil, errors.New("PQ_ADDR not defined")
 	}
 
 	pq_port, succ := os.LookupEnv("PQ_PORT")
-	if !succ || len(strings.TrimSpace(pq_port)) == 0 {
+	if !succ || utils.IsEmpty(pq_port) {
 		return nil, errors.New("PQ_PORT not defined")
 	}
 
 	return &database.DBSettings{
-		Address: pq_addr,
-		Port: pq_port,
-		User: pq_user,
+		Address:  pq_addr,
+		Port:     pq_port,
+		User:     pq_user,
 		Password: pq_passwd,
 		Database: pq_db,
-		}, nil
+	}, nil
+}
 
+func resolveEnv() {
+
+	env, _ := os.LookupEnv("ENV")
+	DEBUG = env == "DEBUG"
+	routes.DEBUG = DEBUG
+
+	if DEBUG {
+		log.Println("Application running in DEBUG env")
+	}
 }
